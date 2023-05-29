646833647ee64970a0e87ee8738de5dc1133bc26
MillionDollarBob <riccardo.c1995@outlook.com>
2023-05-30 01:01:44 +0200

    aggiunta delete

--- La Mia Pizzeria/La Mia Pizzeria/Controllers/API/PizzaApiController.cs
+++ La Mia Pizzeria/La Mia Pizzeria/Controllers/API/PizzaApiController.cs
@@ -15,7 +15,7 @@ Un metodo che dato l'id di una pizza restituisca le info sulla pizza con quel id
         [HttpGet]
         public IActionResult GetPizza()
         {
-            using(PizzaContext db = new PizzaContext())
+            using (PizzaContext db = new PizzaContext())
             {
 
                 List<PizzaModel> pizzaList = db.Pizze.ToList();
@@ -27,9 +27,9 @@ Un metodo che dato l'id di una pizza restituisca le info sulla pizza con quel id
         [HttpGet]
         public IActionResult SearchByName(string name)
         {
-            using(PizzaContext db = new PizzaContext())
+            using (PizzaContext db = new PizzaContext())
             {
-                PizzaModel? PizzaFound = db.Pizze.Where( pizza => pizza.Name.Contains(name)).FirstOrDefault();
+                PizzaModel? PizzaFound = db.Pizze.Where(pizza => pizza.Name.Contains(name)).FirstOrDefault();
                 if (PizzaFound != null)
                 {
                     return Ok(PizzaFound);
@@ -44,9 +44,9 @@ Un metodo che dato l'id di una pizza restituisca le info sulla pizza con quel id
 
         [HttpGet("{id}")]
 
-        public IActionResult SearchById(int id) 
+        public IActionResult SearchById(int id)
         {
-            using(PizzaContext db =new PizzaContext())
+            using (PizzaContext db = new PizzaContext())
             {
                 PizzaModel? PizzaFound = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
                 if (PizzaFound != null)
@@ -59,11 +59,48 @@ Un metodo che dato l'id di una pizza restituisca le info sulla pizza con quel id
                 }
 
             }
-        
+
         }
 
+        [HttpPost]
+        public IActionResult Create([FromBody] PizzaModel pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                using(PizzaContext db = new PizzaContext())
+                {
+                    db.Pizze.Add(pizza);
+                    db.SaveChanges();
 
+                    return Ok();
+                }
+            }
+        }
 
+        [HttpPost("{id}")]
+        public IActionResult Delete(int id)
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                PizzaModel pizza = db.Pizze.Where(pizza => pizza.Id==id).FirstOrDefault();
+
+                if(pizza != null)
+                {
+                    db.Remove(pizza);
+                    db.SaveChanges();
+
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
 
     }
 }

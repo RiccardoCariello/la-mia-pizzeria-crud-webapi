e131a09814a50513a124b00037215b8df1bb6176
MillionDollarBob <riccardo.c1995@outlook.com>
2023-05-29 16:16:44 +0200

    esercizi base

---
+++ La Mia Pizzeria/La Mia Pizzeria/Controllers/API/PizzaApiController.cs
@@ -0,0 +1,69 @@
+﻿using La_Mia_Pizzeria.Database;
+using La_Mia_Pizzeria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace La_Mia_Pizzeria.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaApiController : ControllerBase
+    {
+        /*Deve avere un metodo GetPizzas() che restituisca la lista delle pizze che offre la nostra pizzeria.
+Un metodo per ricercare tramite il titolo della pizza le pizze che hanno nel proprio titolo la stringa specificata
+Un metodo che dato l'id di una pizza restituisca le info sulla pizza con quel id*/
+        [HttpGet]
+        public IActionResult GetPizza()
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+
+                List<PizzaModel> pizzaList = db.Pizze.ToList();
+                return Ok(pizzaList);
+
+            }
+        }
+
+        [HttpGet]
+        public IActionResult SearchByName(string name)
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                PizzaModel? PizzaFound = db.Pizze.Where( pizza => pizza.Name.Contains(name)).FirstOrDefault();
+                if (PizzaFound != null)
+                {
+                    return Ok(PizzaFound);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+
+
+        [HttpGet("{id}")]
+
+        public IActionResult SearchById(int id) 
+        {
+            using(PizzaContext db =new PizzaContext())
+            {
+                PizzaModel? PizzaFound = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+                if (PizzaFound != null)
+                {
+                    return Ok(PizzaFound);
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+            }
+        
+        }
+
+
+
+
+    }
+}
